8cd08cdddf341ff38bea14d44a38a32ddd154413
Baird Rouan S. Buenaventura <bairdbuenaventura@gmail.com>
2019-10-03 18:10:54 +0800

    Create ARSMain.cs

---
+++ COE114L Projectv3/ARSMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COE114L_Projectv3
+{
+    static class ARSMain
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            if (!File.Exists("ARS_Accounts.txt"))
+            {
+                File.Create("ARS_Accounts.txt");
+            }
+            if (!File.Exists("ARS_ReportsList.txt"))
+            {
+                File.Create("ARS_ReportsList.txt");
+            }
+            if (!File.Exists("ARS_CurrentLogin.txt"))
+            {
+                File.Create("ARS_CurrentLogin.txt");
+            }
+            if (!File.Exists("ARS_Help.txt"))
+            {
+                File.Create("ARS_Help.txt");
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LoginForm());
+        }
+    }
+}

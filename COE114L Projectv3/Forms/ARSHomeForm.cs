fc9dcb4454485d8aa8c727ab48e64b3ef9a957d0
Baird Rouan S. Buenaventura <bairdbuenaventura@gmail.com>
2019-10-03 18:10:33 +0800

    Create ARSHomeForm.cs

---
+++ COE114L Projectv3/Forms/ARSHomeForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace COE114L_Projectv3
+{
+    public partial class ARSHomeForm : Form
+    {
+        Account A = new Account();
+        public ARSHomeForm()
+        {
+            InitializeComponent();
+            A.ReadCurrentloginTXT();
+            A.TransferIdentifierDBData();
+            A.ReadAccountsTXT();
+            A.TransferAccountsDBData();
+            label1.Text = "Welcome " + A.prop_Username;
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            TextWriter TW = new StreamWriter("ARS_CurrentLogin.txt", false);
+            TW.WriteLine("");
+            TW.Close();
+            this.Hide();
+            LoginForm LF = new LoginForm();
+            LF.Closed += (s, args) => this.Close();
+            LF.Show();
+        }
+    }
+}
